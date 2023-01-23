e7a1ffce5c1dfc7068e6364eb40d65eee16d92f2
Soo <soojlee0106@einsis.com>
2023-01-23 20:23:04 +0900

    INSERT query

---
+++ mysql_practice/insert.py
@@ -0,0 +1,43 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user=os.getenv("USER"),
+    password=os.getenv("PASSWORD"),
+    database="mydatabase"
+)
+
+mycursor = mydb.cursor()
+
+# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+# val = ("John", "Highway 21")
+# mycursor.execute(sql, val)
+
+# mydb.commit()
+
+sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+val = [
+    ('Peter', 'Lowstreet 4'),
+    ('Amy', 'Apple st 652'),
+    ('Hannah', 'Mountain 21'),
+    ('Michael', 'Valley 345'),
+    ('Sandy', 'Ocean blvd 2'),
+    ('Betty', 'Green Grass 1'),
+    ('Richard', 'Sky st 331'),
+    ('Susan', 'One way 98'),
+    ('Vicky', 'Yellow Garden 2'),
+    ('Ben', 'Park Lane 38'),
+    ('William', 'Central st 954'),
+    ('Chuck', 'Main Road 989'),
+    ('Viola', 'Sideway 1633')
+]
+
+mycursor.executemany(sql, val)
+
+mydb.commit()
+
+print("1 record inserted, ID:", mycursor.lastrowid)
